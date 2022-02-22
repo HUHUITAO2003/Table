696bd3dc3e9a1cc9e88f062ca0b8f9ae5aa33cc2
HU_HUI_TAO <huitao.huangelo@gmail.com>
2022-02-22 22:19:13 +0100

    fine es.table

--- Tabella/js.js
+++ Tabella/js.js
@@ -1,4 +1,5 @@
-var dati = [
+$(function () {
+  var dati = [
     {
       "id": 10001,
       "birthDate": "1953-09-01",
@@ -30,7 +31,7 @@ var dati = [
       "lastName": "Koblick",
       "gender": "M",
       "hireDate": "1986-11-30",
-  
+
     },
     {
       "id": 10005,
@@ -39,45 +40,55 @@ var dati = [
       "lastName": "Maliniak",
       "gender": "M",
       "hireDate": "1989-09-11T22:00:00.000+0000",
-  
+
     }
   ];
 
-  var NextID = 10006; 
+  var NextID = 10006
 
-
-  $(function () {
-    alert("DOM caricato!");
-});
-
-
-function printTable(){
-    var row="";
-    for(var i = 0 ; i < dati.lenght ; i++)
-    {
-        row = row + "<tr>";
-        row = row + "<td>" + dati[i].id + "</td>";
-        row = row + "<td>" + dati[i].firstName + "</td>";
-        row = row + "<td>" + dati[i].lastName + "</td>";
-        row = row + "<td id = " + dati[i].id + "> <button type='button' class='btn btn-danger'>Elimina</button> </td>";
-        row = row + "</tr>"
+  function printTable() {
+    var row = "";
+    for (var i = 0; i < dati.length; i++) {
+      row = row + "<tr>";
+      row = row + "<td>" + dati[i].id + "</td>";
+      row = row + "<td>" + dati[i].firstName + "</td>";
+      row = row + "<td>" + dati[i].lastName + "</td>";
+      row = row + "<td data-id = " + dati[i].id + "> <button type='button' class='btn btn-danger elimina'>Elimina</button> </td>";
+      row = row + "</tr>";
     }
-    alert(row);
-    $(tbody).html(row);
-}
+    $("tbody").html(row);
+  }
+  
+  $("body").ready(function () {
+    printTable();
+  });
 
-$("#aggiungi").bind("click",function (event) {
+  $("#aggiungi").bind("click", function (event) {
+    
+
     var nome = $("#nome").val();
     var cognome = $("#cognome").val();
-    dati.push({"id":NextID,
-        "birthDate": "1953-09-01",
-        "firstName": nome,
-        "lastName": Cognome,
-        "gender": "M",
-        "hireDate": "1986-06-25",
+    
+    dati.push({
+      "id": NextID,
+      "birthDate": "1953-09-01",
+      "firstName": nome,
+      "lastName": cognome,
+      "gender": "M",
+      "hireDate": "1986-06-25",
     })
     NextID++;
     printTable();
+  });
+
+  $(".elimina").bind("click", function (event) {
+    for(var i = 0; i < dati.length ; i++){
+      if(dati[i]==event.data(id)){
+        alert(event.data(id));
+        dati.splice(i,1);
+      }
+    }
+    printTable();
+  });
+
 });
-
-
