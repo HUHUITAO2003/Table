72d1f55e605358028a806a28349fbba9201da777
HU_HUI_TAO <huitao.huangelo@gmail.com>
2022-03-09 14:00:41 +0100

    aggiunta ajax post put delete

--- Tabella/js.js
+++ Tabella/js.js
@@ -1,56 +1,14 @@
-$(function () {/*
-  var dati = [
-    {
-      "id": 10001,
-      "birthDate": "1953-09-01",
-      "firstName": "Georgi",
-      "lastName": "Facello",
-      "gender": "M",
-      "hireDate": "1986-06-25",
-    },
-    {
-      "id": 10002,
-      "birthDate": "1964-06-01",
-      "firstName": "Bezalel",
-      "lastName": "Simmel",
-      "gender": "F",
-      "hireDate": "1985-11-20",
-    },
-    {
-      "id": 10003,
-      "birthDate": "1959-12-02",
-      "firstName": "Parto",
-      "lastName": "Bamford",
-      "gender": "M",
-      "hireDate": "1986-08-27T22:00:00.000+0000",
-    },
-    {
-      "id": 10004,
-      "birthDate": "1954-04-30",
-      "firstName": "Chirstian",
-      "lastName": "Koblick",
-      "gender": "M",
-      "hireDate": "1986-11-30",
-
-    },
-    {
-      "id": 10005,
-      "birthDate": "1955-01-20",
-      "firstName": "Kyoichi",
-      "lastName": "Maliniak",
-      "gender": "M",
-      "hireDate": "1989-09-11T22:00:00.000+0000",
-
-    }
-  ];
-*/
+$(function () {
   var dati;
+  var index="http://localhost:8080/employees";
 
+  //prende i dati da un url
   function getDati(url){
     $.get( url, function(data) {
       dati = data;
       console.log(data);
       printTable(data['_embedded']['employees']);
+
       $("#self").html(data['page']['number']+1);
       if(data['page']['number']==0){
         $(".zero").css("display","none");
@@ -65,6 +23,42 @@ $(function () {/*
     })
   };
 
+  //posta una nuova persona nella tabella
+  function postDati(person){
+    $.ajax({
+      type: "POST",
+      url: index,
+      data: JSON.stringify(person),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data){getDati(dati['_links']['last']['href'])},
+      error: function(errMsg) {console.log(errMsg);}
+  });
+  };
+
+  //elimina una persona nella tabella
+  function deleteDati(id){
+    $.ajax({
+      url: index+'/'+id,
+      type: "delete",
+      success: function(data){getDati(index+"?page="+dati['page']['number']+"&size=20");}
+  })
+  };
+
+  //aggiornamento dati di una persona nella tabella
+  function putDati(person){
+    $.ajax({
+      type: "PUT",
+      url: index+"/"+person.id,
+      data: JSON.stringify(person),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data){getDati(dati['_links']['last']['href']);},
+      error: function(errMsg) {console.log(errMsg);}
+  });
+  };
+
+  //crea la tabella dai dati in input
   function printTable(dati) {
     var row = "";
     for (var i = 0; i < dati.length; i++) {
@@ -78,76 +72,55 @@ $(function () {/*
     $("tbody").html(row);
   }
 
-  
+  //allega l'url al bottone
   $(".pagina").bind("click", function (event) {
     console.log($(this).attr('id'));
     getDati(dati['_links'][$(this).attr('id')]['href']);
   });
 
   $("body").ready(function () {
-    getDati("http://localhost:8080/employees");
+    getDati(index);
   });
-/*
+
+  //aggiunge il metodo postDati al bottono aggiungi della modale
   $("#aggiungi").bind("click", function (event) {
-    
 
     var nome = $("#nome").val();
     var cognome = $("#cognome").val();
-    
-    dati.push({
-      "id": NextID,
-      "birthDate": "",
+    var person ={
+      "birthDate": "2022-03-02",
       "firstName": nome,
+      "gender": "M",
+      "hireDate": "2022-03-02",
       "lastName": cognome,
-      "gender": "Trans",
-      "hireDate": "1986-06-25",
-    })
-    NextID++;
-    printTable();
+    };
+    postDati(person);
   });
 
   $("body").on("click", ".elimina", function (event) {
-    for(var i = 0; i < dati.length ; i++){
-      if(dati[i].id==$(this).parent().attr("data-id")){
-        dati.splice(i,1);
-        break;
-      }
-    }
-    printTable();
+    deleteDati($(this).parent().attr("data-id"));
   });
 
   $("body").on("click", ".edit", function (event) {
-
-    for(var i = 0; i < dati.length ; i++){
-      if(dati[i].id==$(this).parent().attr("data-id")){
-        $("#editnome").val(dati[i].firstName);
-        $("#editcognome").val(dati[i].lastName);
-        $("#salva").val(dati[i].id);
+    for(var i = 0; i < dati['_embedded']['employees'].length ; i++){
+      if(dati['_embedded']['employees'][i].id==$(this).parent().attr("data-id")){
+        $("#editnome").val(dati['_embedded']['employees'][i].firstName);
+        $("#editcognome").val(dati['_embedded']['employees'][i].lastName);
+        $("#salva").val(dati['_embedded']['employees'][i].id);
         break;
       }
     }
-    printTable();
   });
-
 
   $("#salva").bind("click", function (event) {
-    
     var nome = $("#editnome").val();
     var cognome = $("#editcognome").val();
-    for(var i = 0; i < dati.length ; i++){
-      if(dati[i].id==$(this).val()){
-        dati[i]={
-          "id": $(this).val(),
-          "birthDate": "",
-          "firstName": nome,
-          "lastName": cognome,
-          "gender": "Trans",
-          "hireDate": "1986-06-25",
-        }
-        break;
-      }
-    }
-    printTable();
+    $.get( index+"/"+$(this).val(), function(data) {
+      console.log(data);
+      data.firstName=nome;
+      data.lastName=cognome;
+      putDati(data);
+      })
   });
-*/
+
 });
